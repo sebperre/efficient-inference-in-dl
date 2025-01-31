f3971e4efa9e12a4865c295ddd3f7e4ed40c46b2
Sebastien <sebastienlegerperre@gmail.com>
2025-01-30 21:52:23 -0800

    created iterative CIFAR

--- iterative_models/iterativeCIFARVGG.py
+++ iterative_models/iterativeCIFARVGG.py
@@ -8,70 +8,49 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 import numpy as np
 import math
 
-f = open("../logs/CIFAR10RunTime.txt", "a")
+f = open("../logs/CIFAR10Iterative.txt", "a")
 
-f.write(f"===============================================================================")
-f.write(f"Max Testing: Ran at {datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")}\n")
-num_epochs = 100
-f.write(f"Running on VGG Architecture and {num_epochs} epoch(s)\n")
+iterations = 3
+num_epochs = 5
+
+f.write(f"===============================================================================\n")
+f.write(f"Iterative Testing: Ran at {datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")}\n")
+f.write(f"Running on VGG Architecture and {num_epochs} epoch(s)\n\n")
 
 # Followed Geeksforgeeks description: https://www.geeksforgeeks.org/vgg-net-architecture-explained/
 
+layer_configs = {
+    1: [64, "Pool"],
+    2: [64, 64, "Pool"],
+    3: [64, 64, "Pool", 128, 128, "Pool"]
+}
+
+depth_connections = {
+    1: 16384,
+    2: 16384,
+    3: 8192
+}
+
+# Followed the format of https://github.com/chengyangfu/pytorch-vgg-cifar10
+def make_layers(layer_config):
+    layers = []
+    in_channels = 3
+    for v in layer_config:
+        if v == "Pool":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
 class VGG(nn.Module):
-    def __init__(self, num_classes = 10):
+    def __init__(self, depth=1, num_classes = 10):
         super(VGG, self).__init__()
-        self.features = nn.Sequential(
-            # Block 1
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Block 2
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Block 3
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Block 4
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Block 5
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-
+        self.features = make_layers(layer_configs[depth])
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            nn.Linear(512, 512),
+            nn.Linear(depth_connections[depth], 512),
             nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(512, 512),
@@ -112,13 +91,14 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle
 test_dataset = datasets.CIFAR10(root="../data", train=False, download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-def train_model(device, num_epochs):
-    model = VGG().to(device)
+def train_model(device, num_epochs, iteration):
+    model = VGG(iteration).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.05, momentum=0.1, weight_decay=0.0005)
 
     start_time = time.time()
 
+    running_loss = 0.0
     for epoch in range(num_epochs):
         model.train()
         running_loss = 0.0
@@ -133,7 +113,8 @@ def train_model(device, num_epochs):
 
             running_loss += loss.item()
 
-        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(train_loader):.4f}")
+        print(f"Iteration {iteration}: Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(train_loader):.4f}")
+    f.write(f"Loss on Last Iteration for Training is {running_loss}\n")
 
     end_time = time.time()
     return model, end_time - start_time
@@ -181,15 +162,18 @@ def format_time(time):
 if torch.cuda.is_available():
     print("Training on GPU...")
     gpu_device = torch.device("cuda")
-    model, gpu_time = train_model(gpu_device, num_epochs)
 
-    formatted_time = format_time(gpu_time)
+    for iteration in range(1, iterations + 1):
+        model, gpu_time = train_model(gpu_device, num_epochs, iteration)
 
-    print(f"GPU Training Time: {formatted_time}")
-    f.write(f"GPU Training Time: {formatted_time}\n")
+        formatted_time = format_time(gpu_time)
 
-    print("\nTesting on Test Set")
-    test_model(model, gpu_device)
+        print(f"GPU Training Time on Iteration {iteration}: {formatted_time}")
+        f.write(f"GPU Training Time on Iteration {iteration}: {formatted_time}\n")
+
+        print(f"\nIteration {iteration}: Testing on Test Set")
+        f.write(f"Testing on Iteration {iteration}\n")
+        test_model(model, gpu_device)
 else:
     print("GPU not available.")
 
